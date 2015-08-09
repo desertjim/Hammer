d8d82163d37b15d9a037278230e6e73735129718
James Baca <james@jamesbaca.net>
2015-08-09 15:42:59 -0700

    Adding in logic for setting the method type, url, and initial logic for displaying headers

--- app/src/main/java/hammer/jimbaca/net/hammer/CurlBuilder.java
+++ app/src/main/java/hammer/jimbaca/net/hammer/CurlBuilder.java
@@ -1,19 +1,32 @@
 package hammer.jimbaca.net.hammer;
 
+import com.squareup.okhttp.Headers;
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.Request;
 
 public class CurlBuilder {
 
     String method;
+    String url;
+    Headers headers;
 
-    public CurlBuilder(Interceptor.Chain chain){
+    public CurlBuilder(Interceptor.Chain chain) {
         Request request = chain.request();
         method = request.method();
+        url = request.urlString();
+        headers = request.headers();
     }
 
-    public String build(){
+    public String build() {
         StringBuilder builder = new StringBuilder();
+        builder.append("curl ");
+
+        if(headers != null && headers.size() > 0){
+            // Add headers here
+        }
+
+        builder.append("-X " + method + " ");
+        builder.append(url);
 
         return builder.toString();
     }
