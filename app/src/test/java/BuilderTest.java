66409ec671730a1b9b94a63c29554649ebca1075
James Baca <james@jamesbaca.net>
2015-08-19 20:47:40 -0700

    Another pass at a test

--- app/src/test/java/BuilderTest.java
+++ app/src/test/java/BuilderTest.java
@@ -1,6 +1,16 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import hammer.jimbaca.net.hammer.CurlBuilder;
 
 @RunWith(MockitoJUnitRunner.class)
-
 public class BuilderTest {
 
+    @Test
+    public void testEmptyBuilder(){
+        CurlBuilder builder = new CurlBuilder();
+        String value = builder.build();
+        assert value.equals("curl");
+    }
 }
