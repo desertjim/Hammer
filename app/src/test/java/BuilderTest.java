dd2127fc1119fd7aa1991be3c08f691e5aefc689
James Baca <james@jamesbaca.net>
2015-08-22 14:05:28 -0700

    Verifying that the builder generates a valid command

--- app/src/test/java/BuilderTest.java
+++ app/src/test/java/BuilderTest.java
@@ -8,9 +8,17 @@ import hammer.jimbaca.net.hammer.CurlBuilder;
 public class BuilderTest {
 
     @Test
-    public void testEmptyBuilder(){
+    public void testEmptyBuilder() {
         CurlBuilder builder = new CurlBuilder();
         String value = builder.build();
         assert value.equals("curl");
+    }
+
+    @Test
+    public void testMethodSet() {
+        CurlBuilder builder = new CurlBuilder();
+        builder.setMethod("POST");
+        String value = builder.build();
+        assert value.equals("curl -X POST");
     }
 }
